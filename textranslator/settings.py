e92fe2553dad80b4844e267362f33c61b7980467
daria <daria.khud@gmail.com>
2022-01-14 08:22:56 +0100

    'test'

--- textranslator/settings.py
+++ textranslator/settings.py
@@ -140,8 +140,8 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME')
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3BotoStorage'
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3BotoStorage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATIC_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
 ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
