a240af6a15ec83e8e387fa5360e7ac34823ab537
daria <daria.khud@gmail.com>
2022-01-22 14:45:29 +0100

    'commit'

--- textranslator/settings.py
+++ textranslator/settings.py
@@ -148,7 +148,7 @@ django_heroku.settings(locals())
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME')
+AWS_STORAGE_BUCKET_NAME = str(os.environ.get('S3_BUCKET_NAME'))
 
 # AWS_S3_REGION_NAME = 'eu-west-2'
 
