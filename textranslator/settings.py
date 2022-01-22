7a23b6d64f6f11938df6355e2c1a2aa0712e12c1
daria <daria.khud@gmail.com>
2022-01-22 11:35:58 +0100

    'commit'

--- textranslator/settings.py
+++ textranslator/settings.py
@@ -146,12 +146,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 django_heroku.settings(locals())
 
-AWS_ACCESS_KEY_ID = 'AKIAYKYKQ4GPGVPUTK6W '
-# os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = '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'
-# os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'textranslate'
-# os.environ.get('S3_BUCKET_NAME')
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME')
 
 # AWS_S3_REGION_NAME = 'eu-west-2'
 
